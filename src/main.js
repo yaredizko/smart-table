89be6eebbbd2cb15446bfb17e3ff591f37b43694
Kirill Cherkashchenko <yaredizko@yandex.ru>
2026-04-08 22:07:57 +0300

    пр 7.1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import './fonts/ys-display/fonts.css'
 import './style.css'
 
-import {data as sourceData} from "./data/dataset_1.js";
-
 import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
 
